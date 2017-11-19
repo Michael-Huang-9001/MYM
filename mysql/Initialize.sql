e0f7d771a671cdb48f2ae5f55befe27252507b20
YukiKuroshima <yukikuro1993@gmail.com>
2017-11-18 19:05:38 -0800

    :tada: Added Archive implemented with Stored Procedure.

--- mysql/Initialize.sql
+++ mysql/Initialize.sql
@@ -27,7 +27,6 @@ CREATE TABLE User(
         phoneNumber VARCHAR(20),
         income INT,
         agentID INT,
-        updatedAt TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
         userID INT AUTO_INCREMENT,
         FOREIGN KEY(agentID) REFERENCES Agent(agentID),
         PRIMARY KEY(userID)
@@ -58,11 +57,40 @@ CREATE TABLE Appointments(
         agentID INT,
         houseID INT,
         date_time DATETIME,
+        updatedAt TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
         appointmentID INT AUTO_INCREMENT,
         FOREIGN KEY(agentID) REFERENCES Agent(agentID),
         FOREIGN KEY(userID) REFERENCES User(userID),
         PRIMARY KEY(appointmentID)
 );
+
+DROP TABLE IF EXISTS Archive;
+CREATE TABLE Archive(
+        userID INT,
+        agentID INT,
+        houseID INT,
+        date_time DATETIME,
+        updatedAt TIMESTAMP NOT NULL,
+        appointmentID INT NOT NULL,
+        PRIMARY KEY(appointmentID)
+);
+
+DROP PROCEDURE IF EXISTS archiveInactiveAppointments;
+DELIMITER //
+CREATE PROCEDURE archiveInactiveAppointments(IN cutoff VARCHAR(50))
+BEGIN
+        # Insert Appointment data to Archive relation
+        INSERT INTO Archive (userID, agentID, houseID, date_time, updatedAt, appointmentID)
+                # Find appointments that has not been after cutoff date
+                SELECT userID, agentID, houseID, date_time, updatedAt, appointmentID
+                FROM Appointments
+                WHERE updatedAt < cutoff;
+        # Delete archived appointments tuples
+        DELETE
+        FROM Appointments
+        WHERE updatedAt < cutoff;
+END//
+DELIMITER ;
 
 LOAD DATA LOCAL INFILE '/Users/yk/Code/CS157A/project/mysql/RealEstateCompany.txt' INTO TABLE RealEstateCompany;
 LOAD DATA LOCAL INFILE '/Users/yk/Code/CS157A/project/mysql/Agent.txt' INTO TABLE Agent;
