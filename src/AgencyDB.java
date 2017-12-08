4f693f3496c3385142b6b0bce1c26613d9aa4c00
YukiKuroshima <yukikuro1993@gmail.com>
2017-12-07 20:57:26 -0800

    :tada: Done #7

--- src/AgencyDB.java
+++ src/AgencyDB.java
@@ -209,6 +209,92 @@ public class AgencyDB {
 		preparedStatement.setString(2, name);
 		preparedStatement.executeUpdate();
 	}
+	
+	/**
+	 * Get houses by agency name
+	 * @return	void
+	 */
+	public ResultSet searchHouseByAgency(String name) {
+		PreparedStatement preparedStatement = null;
+		String sql = null;
+		ResultSet rs = null;
+
+		sql = "SELECT * FROM House WHERE House.agentID IN "
+				+ "( SELECT agentID FROM Agent WHERE Agent.agencyID IN "
+				+ "( SELECT agencyID FROM RealEstateCompany "
+				+ "WHERE RealEstateCompany.agencyName = ? ));";
+
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, name);
+			rs = preparedStatement.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	/**
+	 * Output the result sets to stdout
+	 * @param rs ResutSet containing agencies' information
+	 */
+	public void displayHouse(ResultSet rs, String successMessage, String errorMessage) {
+		try {
+			rs.beforeFirst();
+			if (!rs.isBeforeFirst()) {
+				System.out.println(errorMessage);
+				return;
+			} else {
+				System.out.println(successMessage);
+				System.out.println("-----------------------");
+				System.out.println(
+						"houseID\t"
+						+ "houseType\t"
+						+ "street\t"
+						+ "city\t"
+						+ "state\t"
+						+ "zipcode\t"
+						+ "year\t"
+						+ "cost\t"
+						+ "bedroomCount\t"
+						+ "bathroomCount\t"
+						+ "squareFeet\t"
+						+ "street"
+						); 
+			}
+			while(rs.next())
+			{
+				String houseType = rs.getString("houseType"); 
+				String street = rs.getString("street"); 
+				String city = rs.getString("city"); 
+				String state = rs.getString("state"); 
+				String zipcode = rs.getString("zipcode"); 
+				int year = rs.getInt("year"); 
+				int cost = rs.getInt("cost"); 
+				int bedroomCount = rs.getInt("bedroomCount"); 
+				int bathroomCount = rs.getInt("bathroomCount"); 
+				double squareFeet = rs.getDouble("squareFeet"); 
+				int houseID = rs.getInt("houseID"); 
+
+				System.out.println(
+						houseID + "\t" + 
+						houseType + "\t" + 
+						street + "\t" + 
+						city + "\t" + 
+						state + "\t" + 
+						zipcode + "\t" + 
+						year + "\t" + 
+						cost + "\t" + 
+						bedroomCount + "\t" + 
+						bathroomCount + "\t" + 
+						squareFeet 
+						); 
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 
 	private static void printResultSetfromFaculty(ResultSet rs) throws SQLException
 	{
